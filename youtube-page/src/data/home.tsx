c456c457ed5740e7b175911cad5b5c400971ab0d
Charvi Khurana <67728901+charvi03@users.noreply.github.com>
2024-10-20 13:41:50 +0530

    Add files via upload

---
+++ youtube-page/src/data/home.tsx
@@ -0,0 +1,179 @@
+export const categories = [
+  "All",
+  "JavaScript",
+  "TypeScript",
+  "Programming",
+  "Comedy",
+  "News",
+  "Podcasts",
+  "Drama",
+  "React",
+  "Next.js",
+  "Object Oriented Programming",
+  "Frontend Web Development",
+  "Backend Web Development",
+  "Web Development",
+  "Wealth",
+];
+export const videos = [
+  {
+    id: "1",
+    title: "How AI Is Revolutionizing Healthcare",
+    channel: {
+      name: "TechNow",
+      id: "TechChannel123",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 543890,
+    postedAt: new Date("2024-01-10"),
+    duration: 938,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "2",
+    title: "Exploring the Future of Quantum Computing",
+    channel: {
+      name: "TechNow",
+      id: "TechChannel123",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 763200,
+    postedAt: new Date("2023-12-18"),
+    duration: 1020,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "3",
+    title: "5G Technology: What You Need to Know",
+    channel: {
+      name: "TechNow",
+      id: "TechChannel123",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 122550,
+    postedAt: new Date("2023-11-22"),
+    duration: 780,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "4",
+    title: "The Rise of Blockchain in Finance",
+    channel: {
+      name: "TechNow",
+      id: "TechChannel123",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 321940,
+    postedAt: new Date("2023-10-05"),
+    duration: 900,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "5",
+    title: "Electric Cars: The Future of Transportation",
+    channel: {
+      name: "TechNow",
+      id: "TechChannel123",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 452340,
+    postedAt: new Date("2024-02-14"),
+    duration: 820,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "6",
+    title: "Virtual Reality: Redefining Entertainment",
+    channel: {
+      name: "TechNow",
+      id: "TechChannel123",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 978400,
+    postedAt: new Date("2023-08-10"),
+    duration: 680,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "7",
+    title: "How Drones Are Changing the World",
+    channel: {
+      name: "TechNow",
+      id: "TechChannel123",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 543210,
+    postedAt: new Date("2024-03-02"),
+    duration: 945,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "8",
+    title: "The Impact of Social Media on Society",
+    channel: {
+      name: "TechNow",
+      id: "TechChannel123",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 815670,
+    postedAt: new Date("2023-12-29"),
+    duration: 860,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "9",
+    title: "Understanding Machine Learning in 5 Minutes",
+    channel: {
+      name: "TechNow",
+      id: "TechChannel123",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 452130,
+    postedAt: new Date("2024-01-05"),
+    duration: 600,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+  {
+    id: "10",
+    title: "Understanding THE NEED OF TYPESCRIPT",
+    channel: {
+      name: "TechNow",
+      id: "TechChannel123",
+      profileUrl:
+        "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+    },
+    views: 452130,
+    postedAt: new Date("2023-01-05"),
+    duration: 600,
+    thumbnailUrl: "https://i.ytimg.com/vi/kxT8-C1vmd4/maxresdefault.jpg",
+    videoUrl:
+      "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+  },
+];
---
+++ youtube-page/src/data/sidebar.ts
@@ -0,0 +1,40 @@
+export const subscriptions = [
+  {
+    channelName: "Tech1",
+    id: "TraversyMedia",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKYcYswt_UhD7D0j6ddiQz6Gb8Q_vSJOjhYI0CoXSw=s176-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    channelName: "Tech2",
+    id: "TraversyMedia",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKYcYswt_UhD7D0j6ddiQz6Gb8Q_vSJOjhYI0CoXSw=s176-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    channelName: "Tech3",
+    id: "TraversyMedia",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKYcYswt_UhD7D0j6ddiQz6Gb8Q_vSJOjhYI0CoXSw=s176-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    channelName: "Tech4",
+    id: "TraversyMedia",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKYcYswt_UhD7D0j6ddiQz6Gb8Q_vSJOjhYI0CoXSw=s176-c-k-c0x00ffffff-no-rj-mo",
+  },
+  {
+    channelName: "Tech5",
+    id: "TraversyMedia",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKYcYswt_UhD7D0j6ddiQz6Gb8Q_vSJOjhYI0CoXSw=s176-c-k-c0x00ffffff-no-rj-mo",
+  },
+];
+
+export const playlists = [
+  { id: "1", name: "Frontend & Backend" },
+  { id: "2", name: "Health" },
+  { id: "3", name: "Funny" },
+  { id: "4", name: "Non-Dev" },
+  { id: "5", name: "Work" },
+];
